804f694948c76f81b3127278d920927e48f3a932
Diego Navarro <dnmellen@gmail.com>
2013-11-21 20:33:02 +0100

    Adds templates for production configuration and bumbs version number

--- bulkymonkey/bulkymonkey/settings.py
+++ bulkymonkey/bulkymonkey/settings.py
@@ -97,6 +97,7 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
 
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
